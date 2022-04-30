b38415273aba6d8f92b8e61b4ce610c7256ef92e
sahersamara <95171462+sahersamara@users.noreply.github.com>
2022-04-30 23:14:32 +0300

    g

--- src/p1/AddAnimalDialog.java
+++ src/p1/AddAnimalDialog.java
@@ -1,3 +1,9 @@
+/**
+ * Student 1 Name: Malak Ghrayeb
+ * Id 1: ***REMOVED***
+ * Student 2 Name: Saher Samara
+ * Id 2: ***REMOVED***
+ */
 package p1;
 
 import javax.swing.*;
--- src/p1/Swimmable.java
+++ src/p1/Swimmable.java
@@ -1,6 +1,8 @@
 /**
- * Name: Malak Ghrayeb
- * Id: ***REMOVED***
+ * Student 1 Name: Malak Ghrayeb
+ * Id 1: ***REMOVED***
+ * Student 2 Name: Saher Samara
+ * Id 2: ***REMOVED***
  */
 package p1;
 
